df00275153ae972f95e554fcf1435b109edd47da
Wanley Rooby Alexis <alexroob96@gmail.com>
2025-12-10 16:44:49 -0300

    primeiro commit

---
+++ src/main/java/com/igrejaplus/IgrejaplusApplication.java
@@ -0,0 +1,13 @@
+package com.igrejaplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrejaplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgrejaplusApplication.class, args);
+	}
+
+}
